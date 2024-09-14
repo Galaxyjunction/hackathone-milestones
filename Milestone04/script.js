b196c647f046563a92000e5a4eccf8002af9761f
Galaxy-Junction <161058155+Galaxyjunction@users.noreply.github.com>
2024-09-14 12:03:16 +0500

    Add files via upload

---
+++ Milestone04/script.js
@@ -0,0 +1,20 @@
+// Get refrences----
+var formForResume = document.getElementById('resume-form');
+var resume_Display_Element = document.getElementById('resume-display');
+//Handle form submission
+formForResume.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skills = document.getElementById('skills').value;
+    var resumeHTML = "\n    <h2><b>Editable Resume</b></h2>\n    <h3>Personal Information</h3><b>\n    <p><b>Name:</b><span contenteditable=\"true\">".concat(name, "</span></p>\n    <p><b>Email:</b><span contenteditable=\"true\">").concat(email, "</span></p>\n    <p><b>Phone:</b><span contenteditable=\"true\">").concat(phone, "</span></p>\n\n    <h3>Education</h3>\n    <p contenteditable=\"true\">").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p contenteditable=\"true\">").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p contenteditable=\"true\">").concat(skills, "</p>\n    ");
+    if (resume_Display_Element) {
+        resume_Display_Element.innerHTML = resumeHTML;
+    }
+    else {
+        console.error('The resume display element is missing.');
+    }
+});
---
+++ Milestone04/script.ts
@@ -0,0 +1,39 @@
+// Get refrences----
+
+const formForResume = document.getElementById('resume-form') as HTMLFormElement
+const resume_Display_Element = document.getElementById('resume-display') as HTMLDivElement
+
+//Handle form submission
+formForResume.addEventListener('submit', (event: Event)=>{
+    event.preventDefault();
+
+    const name = (document.getElementById('name') as HTMLInputElement).value
+    const email = (document.getElementById('email') as HTMLInputElement).value
+    const phone = (document.getElementById('phone') as HTMLInputElement).value
+    const education = (document.getElementById('education') as HTMLInputElement).value
+    const experience = (document.getElementById('experience') as HTMLInputElement).value
+    const skills = (document.getElementById('skills') as HTMLInputElement).value
+    
+    const resumeHTML = `
+    <h2><b>Editable Resume</b></h2>
+    <h3>Personal Information</h3><b>
+    <p><b>Name:</b><span contenteditable="true">${name}</span></p>
+    <p><b>Email:</b><span contenteditable="true">${email}</span></p>
+    <p><b>Phone:</b><span contenteditable="true">${phone}</span></p>
+
+    <h3>Education</h3>
+    <p contenteditable="true">${education}</p>
+
+    <h3>Experience</h3>
+    <p contenteditable="true">${experience}</p>
+
+    <h3>Skills</h3>
+    <p contenteditable="true">${skills}</p>
+    `;
+
+    if(resume_Display_Element){
+        resume_Display_Element.innerHTML = resumeHTML;
+    }else{
+        console.error('The resume display element is missing.')
+    }
+})
